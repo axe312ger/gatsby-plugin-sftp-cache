594141863b58e0e66ecb8303d4f8129009447ae9
Benedikt Rötsch <opensource@axe312.dev>
2020-05-25 23:40:23 +0200

    properly execute diff and catch and render errors

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,33 +1,43 @@
 const sftpCache = require('sftp-cache')
 
-exports.onPreInit = async function(
+exports.onPreInit = async function (
   { store },
   { connection, remoteDir, dirsToCache, concurrency = 2 }
 ) {
   const program = store.getState().program
   const localDir = program.directory
-  await sftpCache({
-    connection,
-    remoteDir,
-    dirsToCache,
-    localDir,
-    concurrency,
-    syncDirection: 'download'
-  })
+  try {
+    await sftpCache({
+      connection,
+      remoteDir,
+      dirsToCache,
+      localDir,
+      concurrency,
+      syncDirection: 'download'
+    })
+  } catch (err) {
+    console.log(`Failed to download files from SFTP server.`)
+    console.error(err)
+  }
 }
 
-exports.onPostBuild = async function(
+exports.onPostBuild = async function (
   { store },
   { connection, remoteDir, dirsToCache, concurrency = 2 }
 ) {
   const program = store.getState().program
   const localDir = program.directory
-  await sftpCache({
-    connection,
-    remoteDir,
-    dirsToCache,
-    localDir,
-    concurrency,
-    syncDirection: 'cache'
-  })
+  try {
+    await sftpCache({
+      connection,
+      remoteDir,
+      dirsToCache,
+      localDir,
+      concurrency,
+      syncDirection: 'cache'
+    })
+  } catch (err) {
+    console.log(`Failed to cache files to SFTP server.`)
+    console.error(err)
+  }
 }
